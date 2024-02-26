fe64883433adddc1dec1b9845a685b769f075aba
Skolnieks <skolnieks.pjvt@pjvt.local>
2024-02-26 08:56:21 +0200

    first commit

---
+++ db.sql
@@ -0,0 +1,10 @@
+ -- CREATE DATABASE blog_Liepins;
+ -- USE DATABASE blog_Liepins;
+/*CREATE TABLE posts (
+id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+title VARCHAR(255) NOT NULL
+);
+
+INSERT INTO posts (title) VALUES
+("My First Blog Post"),("My Second Blog Post");
+*/
