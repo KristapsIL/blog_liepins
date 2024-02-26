0fb59b059990e9c727a7a0325fd2c87db47a1465
Skolnieks <skolnieks.pjvt@pjvt.local>
2024-02-26 09:47:47 +0200

    aa

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+echo "hai, hai";
+
+$connection_string = "mysql:host=localhost;dbname=blog_liepins; user=root; password=; charset=utf8mb4";
+$pdo = new PDO($connection_string);
+
+$query = $pdo->prepare("SELECT * FROM posts");
+$query->execute();
+$posts = $query->fetchAll(PDO::FETCH_ASSOC);
+echo '<pre>';
+var_dump($posts);
+echo '</pre>';
