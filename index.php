bee222aebf7e28420543b35b1dc66fdc442e1974
Skolnieks <skolnieks.pjvt@pjvt.local>
2024-02-27 13:26:34 +0200

    aa

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-echo "hai, hai";
+require "functions.php";
 
 $connection_string = "mysql:host=localhost;dbname=blog_liepins; user=root; password=; charset=utf8mb4";
 $pdo = new PDO($connection_string);
@@ -7,6 +7,9 @@ $pdo = new PDO($connection_string);
 $query = $pdo->prepare("SELECT * FROM posts");
 $query->execute();
 $posts = $query->fetchAll(PDO::FETCH_ASSOC);
-echo '<pre>';
-var_dump($posts);
-echo '</pre>';
+
+echo "<ol>";
+foreach($posts as $post){
+    echo "<li>".$post["title"]."</li>";
+}
+echo "</ol>";
