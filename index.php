2fb98ffb7aa2127638817ae02c0dc206d2c0d303
Skolnieks <skolnieks.pjvt@pjvt.local>
2024-03-12 13:30:29 +0200

    tis

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 require "functions.php";
 require "Database.php";
+
 $config = require ("config.php");
 
 $query = "SELECT * FROM posts";
@@ -19,36 +20,7 @@ $db = new Database($config);
 $posts = $db
     ->execute($query, $params)
     ->fetchAll();
-
-echo "<form>";
-echo "<input name='id' value='". ($_GET["id"] ?? '')."'/>";
-echo "<button >Submit</button>";
-echo "</form>";
-
-echo "<p>categories</p>";
-
-echo "<form>";
-echo "<input name='name' value='". ($_GET["name"] ?? '')."'/>";
-echo "<button >Submit</button>";
-echo "</form>";
-
-echo "<form>";
-echo "<select name='name'/>";
-echo "<option value='sport'>sport</option>";
-echo "<option value='music'>music</option>";
-echo "<option value='food'>food</option>";
-echo "</select>";
-echo "<button >Submit</button>";
-echo "</form>";
-
-echo "<h1> POSTS </h1>";
-echo "<ul>";
-foreach($posts as $post){
-    echo "<li>".$post["title"]."</li>";
-}
-echo "</ul>";
-
-
+require "index.view.php";
 
 
 
