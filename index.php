23f4e01f135c3286e5547d1a7d06a8cc5af981c3
Skolnieks <skolnieks.pjvt@pjvt.local>
2024-03-15 09:04:47 +0200

    good

--- index.php
+++ index.php
@@ -1,17 +1,5 @@
 <?php
 
-$url_array = parse_url($_SERVER["REQUEST_URI"]);
-$url = $url_array["path"];
+require "router.php";
 
-if($url ==  "/about"){
-    require "controllers/about.php";
-}
-else if($url ==  "/index" || $url ==  "/"){
-    require "controllers/index.php";
-}
-else if($url ==  "/story"){
-    require "controllers/story.php";
-} else{
-    http_response_code(404);
-    require "controllers/404.php";
-}
+
--- views/components/navbar.php
+++ views/components/navbar.php
@@ -1,6 +1,6 @@
 <header>
     <nav>
-        <a href="index">Main</a>
+        <a href="/">Main</a>
         <a href="about">About</a>
         <a href="story">Story</a>
     </nav>
